71390c1aa14d44fba447d763cde54aeef5fe750e
Nikolay Iliev <nikolay.iliev@telerik.com>
2021-06-09 11:06:34 +0300

    Add project files.

---
+++ FiddlerCore_Brotli_NetCore/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Fiddler;
+using Brotli;
+
+namespace FiddlerCore_DecodeBrotli_NetFramework
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Custom cert provider can be used.
+            BCCertMaker.BCCertMaker certProvider = new BCCertMaker.BCCertMaker();
+            CertMaker.oCertProvider = certProvider;
+
+            FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
+
+            if (!CertMaker.createRootCert())
+            {
+                Console.WriteLine("Unable to create cert for FiddlerCore.");
+                return;
+            }
+
+            if (!CertMaker.trustRootCert())
+            {
+                Console.WriteLine("Unable to install FiddlerCore's cert.");
+                return;
+            }
+
+            FiddlerCoreStartupSettings startupSettings =
+                                            new FiddlerCoreStartupSettingsBuilder()
+                                                .ListenOnPort(8887)
+                                                .DecryptSSL()
+                                                .RegisterAsSystemProxy()
+                                                .Build();
+
+
+            FiddlerApplication.Startup(startupSettings);
+
+
+            Console.WriteLine("\nPROXY IS NOW SET");
+            Console.WriteLine("To remove the proxy and close the app, press Enter");
+
+            Console.ReadLine();
+            FiddlerApplication.Shutdown();
+        }
+
+        private static void FiddlerApplication_AfterSessionComplete(Session oSession)
+        {
+            if (oSession.ResponseHeaders.ExistsAndContains("content-encoding", "br") && oSession.responseBodyBytes.Length > 0)
+            {
+                var decodedBytes = oSession.responseBodyBytes.DecompressFromBrotli();
+                var text = Encoding.Default.GetString(decodedBytes);
+                Console.WriteLine("Decoded body is: " + text);
+            }
+        }
+    }
+}
